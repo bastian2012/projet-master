120480c6fbe6d9cea72c031955012c8b20fdbebc
Bastien Jean Ritchy <bjeanritchy@gmail.com>
2023-09-07 16:58:46 -0400

    commit 1

---
+++ PRINCIPALE.php
@@ -0,0 +1,202 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+<!-- Vendor CSS Files -->
+<link href="vendor/animate.css/animate.min.css" rel="stylesheet">
+<link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+<link href="vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+<link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+<link href="vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+<link href="vendor/remixicon/remixicon.css" rel="stylesheet">
+<link href="vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+<!-- Template Main CSS File -->
+<link href="css/style.css" rel="stylesheet">
+</head>
+</head>
+<body>
+<div id="topbar" class="d-flex align-items-center fixed-top">
+  <div class="container d-flex justify-content-between">
+    <div class="contact-info d-flex align-items-center">
+      <i class="bi bi-envelope"></i> <a href="mailto:contact@example.com">prcare@example.com</a>
+      <i class="bi bi-phone"></i> +509 2941 1101
+    </div>
+    <div class="d-none d-lg-flex social-links align-items-center">
+      <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+      <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+      <a href="#" class="twitter"><i class="bi bi-twitter"></i></a> 
+      <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></i></a>
+    </div>
+  </div>
+</div>
+
+  <header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">OFATMA</a></h1>
+    
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#home">Accueil</a></li>
+          <li><a class="nav-link scrollto" href="#about">A-Propos</a></li>
+          <li><a class="nav-link scrollto" href="#services">Services</a></li>
+          <li><a class="nav-link scrollto" href="#doctors">Docteurs</a></li>       
+          <li><a class="nav-link scrollto" href="#contact">Contacts</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav>
+
+      <a href="#appointment" class="appointment-btn scrollto"><span class="d-none d-md-inline">Faire une</span> Consultation</a>
+
+    </div>
+  </header>
+
+  
+  <section id="home" class="d-flex align-items-center">
+    <div class="container">
+      <h1>Bienvenue sur le site de<br> L'OFATMA</h1>
+      <h2>L'OFATMA a pour premier objectif d'apporter à tous les salariés une protection efficace et automatique contre les risques d'accidents de travail et assimilés.<br>"Votre sante mental el physique sont notre priorite"</h2>
+      <a href="#about" class="btn-get-started scrollto">Lire plus</a>
+    </div>
+  </section><!-- End Home -->
+<br><br><br><br><br><br><br><br>
+  <main id="main">
+      <!-- ======= Featured Services Section ======= -->
+    <section id="featured-services" class="featured-services">
+      <div class="container" data-aos="fade-up">
+
+        <div class="row">
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="100">
+              <div class="icon"><i class="fas fa-heartbeat"></i></div>
+              <h4 class="title"><a href=""> Normal checkup</a></h4>
+              <p class="description">Apaiser et corrompre les plaisirs 2 fois par mois pour rattraper les contrariétés</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="400">
+              <div class="icon"><i class="fas fa-dna"></i></div>
+              <h4 class="title"><a href="">Blood Test</a></h4>
+              <p class="description">Une prise de sang est un prélèvement sanguin réalisé le plus souvent dans une veine du bras, à l’aide d’une aiguille. Il permet d’analyser le sang, pour diagnostiquer certaines maladies.</p>
+            </div>
+          </div>
+          
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="300">
+              <div class="icon"><i class="fas fa-temperature-high"></i></div>
+              <h4 class="title"><a href="">Température corporelle</a></h4>
+              <p class="description">Excepteur sint occaecat 37.6 non proident, 98.6 sunt in culpa qui officia</p>
+            </div>
+          </div>
+
+          <div class="col-md-6 col-lg-3 d-flex align-items-stretch mb-5 mb-lg-0">
+            <div class="icon-box" data-aos="fade-up" data-aos-delay="200">
+              <div class="icon"><i class="fas fa-hand-holding-medical"></i></div>
+              <h4 class="title"><a href="">Utilisation de médicaments</a></h4>
+              <p class="description">Le médicament est d'abord là pour soigner mais s'il est mal utilisé, il peut être dangereux pour la santé. En effet, il contient des substances actives qui ont des effets sur l'organisme afin de traiter une maladie mais des effets indésirables peuvent survenir. «Toute prise de médicament comporte des risques potentiels mais ça vaut toujours le coup d'en prendre quand on en a réellement besoin», explique Bernard Bégaud, médecin et pharmacologue, spécialiste des médicaments et de leurs effets. En revanche, «si l'on prend un médicament sans en avoir besoin, les risques deviennent inacceptables!»</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Featured Services Section -->
+     <!-- ======= About Section ======= -->
+     <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
+        <div class="section-title">
+          <h2>A-Propos</h2>
+          <p>• Selon la loi du 28 août 1967, l’OFATMA est une institution publique
+autonome placée sous l’autorité de tutelle du Ministère des Affaires
+Sociales et du Travail (MAST).</p>
+<P>
+* Mandat statutaire de fourniture de Services d’Assurance Accidents
+du Travail, Maladie et Maternité aux employés de la Fonction
+Publique et aux travailleurs du secteur privé.
+</P>
+<p>
+• Couverture obligatoire des salariés et de leurs familles contre les
+risques d’accidents du travail, de maladie et de maternité,
+moyennant les cotisations du patronat et des salariés.
+</p>
+
+ <h2> VISION DU GOUVERNEMENT </h2>
+<h3> Engagement solennel du Président Michel Joseph Martelly</h3>
+<p>« …un Système National de Protection Sociale en Santé qui
+concernera à terme tous les habitants d’Haïti … financé
+progressivement par les ressources propres du pays afin de nous
+rendre maîtres de notre système de soins»
+( discours sur l’Etat de la Nation ,14 janvier 2013).
+</p>
+<h2>NATURE, PRINCIPES</h2>
+<p>• Le système d’assurance de l’OFATMA est d’essence solidaire,
+articulée à la protection sociale</p>
+<p>
+• Le système est basé sur des cotisations liées au travail, que
+celui-ci soit formel ou informel.
+</p>
+<p>
+• La priorisation de la mobilisation des ressources nationales
+pour réduire la dépendance aux aides externes
+</p>
+<p>
+• Ce mécanisme concernera à terme, progressivement, tous les
+haïtiens
+</p>
+<p>
+• La qualité des prestation des services est au centre de la
+démarche
+</p>
+<p>
+• Le Système de Sécurité Sociale (volet santé) : généralisation à
+tous les fonctionnaires puis au reste de la population.
+
+</p>
+        </div>
+
+    <div class="row">
+    <div class="col-xl-5 col-lg-6 video-box d-flex justify-content-center
+     align-items-stretch position-relative">
+    <a href="https://www.youtube.com/watch?v=jkuZZJVDgDs" class="glightbox play-btn mb-4"></a>
+          </div>
+
+    <div class="col-xl-7 col-lg-6 icon-boxes d-flex flex-column align-items-stretch
+     justify-content-center py-5 px-lg-5">
+            
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-heart"></i></div>
+              <h4 class="title"><a href="">La sante est la vraie richesse</a></h4>
+              <p class="description">Sans la santé, la richesse ne servirait pas à grand-chose. Que pourrait-on faire avec tout l'argent du monde face à une maladie incurable ?</p>
+            </div>
+
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-health"></i></div>
+              <h4 class="title"><a href="">EMERGENCY</a></h4>
+              <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+            </div>
+
+            <div class="icon-box">
+              <div class="icon"><i class="bx bx-help-circle"></i></div>
+              <h4 class="title"><a href="">SERVICES </a></h4>
+              <p class="description">Explicabo est voluptatum asperiores consequatur magnam. Et veritatis odit. Sunt aut deserunt minus aut eligendi omnis</p>
+            </div>
+            
+          </div>
+        </div>
+
+      </div>
+    </section><!-- End About Section -->
+    
+</body>
+</html>
+?>
